4c0d896bb5a38e05476142cf700c8bb89b88b2d1
vi xent <alagovincent03@gmailcom>
2023-04-23 13:48:24 -0700

    first commit

--- classes/person.js
+++ classes/person.js
@@ -1,10 +1,34 @@
 // Your code here
+class Person {
+    constructor(firstname, lastname, age) {
+        this.firstname = firstname
+        this.lastname = lastname
+        this.age = age;
+    }
+    introduce() {
+        return `Hi i'm ${this.firstname} ${this.lastname} and i'm ${this.age} years old`;
+
+    }
+
+    static introducePersons(...Person) {
+        Person.forEach(element => {
+
+            if (element !== typeof(Person)) {
+                console.log(`Error:  All items in array must be Person class instances.`)
+            } else {
+                Person.introduce()
+            }
+        })
+    }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports = Person;
+    module.exports = Person;
 } catch {
-  module.exports = null;
+    module.exports = null;
 }
